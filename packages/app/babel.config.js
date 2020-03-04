4fa8233002de8827b9df0688200aff6b61b538c4
Jean Leonço <jeang.leonco@gmail.com>
2020-03-03 21:38:54 -0300

    feat(app): add app package

---
+++ packages/app/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-export-default-from',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'relay',
+    'optional-require',
+    'babel-plugin-idx',
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  sourceMaps: true,
+};
